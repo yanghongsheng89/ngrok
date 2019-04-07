138016e518554c7c53680dc6d40278546a411d87
Jack <yanghongsheng89@163.com>
2019-04-07 16:18:44 +0800

    new file:   cert/cret.sh

---
+++ cert/cret.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export NGROK_DOMAIN="yuproxy.vip"
+
+openssl genrsa -out rootCA.key 2048
+
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+
+openssl genrsa -out device.key 2048
+
+openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
+
+openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+
+echo "y"|cp rootCA.pem ../assets/client/tls/ngrokroot.crt
+
+echo "y"|cp device.crt ../assets/server/tls/snakeoil.crt
+
+echo "y"|cp device.key ../assets/server/tls/snakeoil.key
